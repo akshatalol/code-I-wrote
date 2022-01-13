ed70c3ad24368f30bde36f89faac9228a65dc56f
Akshata <44158331+akshatalol@users.noreply.github.com>
2022-01-13 17:14:09 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
-
+#!usr/bin/env python3
 import subprocess
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:45", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+
+interface = input("Enter the interface name you want to change the MAC address for:")
+new_mac = input("Enter the new MAC address in this form: 00:aa:bb:cc:dd:ee where a,b,c,d,e are alphanumeric characters:")
+print("Changing the MAC address of "+interface+" to "+new_mac)
+subprocess.call(["ifconfig", interface, "down"]) #list method is inefficient, but helps sanitise user input to avoid code injections
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
